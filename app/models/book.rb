a6a71bd8459b0064332c0e1fb2dc8769fa70a710
piyoppa <piyoppa@gmail.com>
2020-12-25 00:31:59 +0900

    Add tables

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
     scope :costly, -> {where("price > ?", 3000)}
     scope :written_about, -> (theme) {where("name like ?", "%#{theme}")}
     default_scope -> {order("published_on desc")}
+
+    belongs_to :publisher
+    has_many :book_authors
+    # 中間テーブルの指定をthroughでしている
+    has_many :authors, through: :book_authors
 end
